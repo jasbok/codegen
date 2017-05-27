d791af909b679d3941fd9061283876dced42b0f5
Stefan Alberts <stefan6573@gmail.com>
2017-05-27 23:25:12 +0200

    [watch] Added functionality to check for files changes in specified dirs.

--- codegen.py
+++ codegen.py
@@ -3,7 +3,7 @@
 '''
 MIT License
 
-Copyright (c) [2017] [Stefan Alberts]
+Copyright (c) 2017 Stefan Alberts
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -24,10 +24,14 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
+import datetime
+import glob
 import json
 import os
 import re
+import subprocess
 import sys
+import time
 
 R_SCOPE = "(\$\$|!!|\^\^|@@|%%)"
 R_PATH = "(?:\.([\w|\.]*))?"
@@ -47,42 +51,96 @@ class Codegen(object):
         self._templates = {}
         self._scopes = [[]]
 
+        self._watched_dirs = []
+        self._watched_files = {}
+        self._recently_modified = {}
+
+        self._do_print = False
+        self._do_watch = False
+
+        self._watch_interval = 15
+        self._recent_interval = 2
+        self._recent_ttl = 300
+        self._watch_interval_counter = 0
+
     def add_schema(self, schema):
         self._schemas[schema] = self._load_schema(schema)
 
     def add_template(self, template):
         self._templates[template] = self._file_contents(template)
 
-    def process(self):
-        for schema in self._schemas.values():
-            for template in self._templates.values():
+    def add_watched(self, dir):
+        self._watched_dirs.append(dir)
+
+    def print_to_stdout(self):
+        self._do_print = True
+
+    def start(self):
+        if len(self._watched_dirs) > 0:
+            running = True
+            while running:
+                if self._watch_interval_counter == 0:
+                    modified = self._get_modified_files()
+                    self._watch_interval_counter = self._watch_interval
+                else:
+                    modified = self._check_recently_modified()
+
+                if len(modified) > 0:
+                    print(modified)
+                try:
+                    self._watch_interval_counter -= 1
+                    time.sleep(self._recent_interval)
+                except KeyboardInterrupt:
+                    running = False
+        else:
+            self.process(self._schemas.values(), self._templates.values())
+
+    def process(self, schemas, templates):
+        for schema in schemas:
+            for template in templates:
                 compiled = self._compile(template, schema)
-                print(compiled)
+
+                if self._do_print:
+                    print(compiled)
 
     def _compile(self, template, schema):
         result = ""
         content = template
 
         while True:
-            token = REGEX_TOKEN.search(content)
-            if token is None:
+            match = REGEX_TOKEN.search(content)
+            if match is None:
                 result += content
                 break
 
-            compiled = ""
-            token_info = self._get_token_info(token)
+            result += content[:match.start()]
+            content = self._process_token(match, schema) + content[match.end():]
 
-            self._push_scope(token_info["operator"], token_info["path"])
+        return result
+
+    def _process_token(self, match, schema):
+        token = self._get_token(match)
+        op = token["operator"]
+        if op == "$$" or op == "!!" or op == "^^":
+            result = self._process_schema_token(token, schema)
+        elif op == "%%":
+            result = self._process_function_token(token)
+        elif op == "@@":
+            result = self._load_template(token["path"])
+
+        return result or ""
+
+    def _process_schema_token(self, token, schema):
+            compiled = ""
+            self._push_scope(token["operator"], token["path"])
             var = self._get_var(schema, self._scopes[-1])
             if var is not None:
-                if token_info["expansion"] is not None:
-                    select = token_info["select"]
+                if token["expansion"] is not None:
+                    select = token["select"]
                     if isinstance(var, list):
-                        compiled = ""
-
                         for index in self._get_selected_indices(var, select):
                             self._scopes[-1].append(index)
-                            compiled += self._compile(token_info["expansion"], schema)
+                            compiled += self._compile(token["expansion"], schema)
                             self._scopes[-1].pop()
                     else:
                         compile = select is None \
@@ -91,25 +149,36 @@ class Codegen(object):
                             or isinstance(var, float) and var == float(select) \
                             or isinstance(var, str) and var == select
                         if compile:
-                            compiled = self._compile(token_info["expansion"], schema)
+                            compiled = self._compile(token["expansion"], schema)
 
                 else:
                     compiled = str(var)
             self._pop_scope()
+            return compiled
 
-            result += content[:token.start()]
-            content = compiled + content[token.end():]
+    def _process_function_token(self, token):
+        path = token["path"]
 
-        return result
+        if path == "date":
+            return datetime.datetime.now().strftime(token["expansion"])
+        elif path == "git.name":
+            return self._git_config("user.name")
+        elif path == "git.email":
+            return self._git_config("user.email")
+        return ""
 
-    def _get_token_info(self, token):
-        info = {
-            "operator": token.group(1),
-            "path": token.group(2),
-            "select": token.group(3),
-            "expansion": token.group(4),
+    def _git_config(self, property):
+        return subprocess.check_output(["git", "config", property])     \
+            .decode("utf-8")                                            \
+            .replace("\n", "")
+
+    def _get_token(self, match):
+        return {
+            "operator": match.group(1),
+            "path": match.group(2),
+            "select": match.group(3),
+            "expansion": match.group(4),
         }
-        return info
 
     def _get_selected_indices(self, arr, select):
         indices = range(len(arr))
@@ -162,10 +231,10 @@ class Codegen(object):
         if isinstance(var, str):
             match = REGEX_TOKEN.match(var)
             if match:
-                token_info = self._get_token_info(match)
+                token = self._get_token(match)
 
-                if token_info["operator"] == "@@":
-                    var = self._load_template(token_info["path"])
+                if token["operator"] == "@@":
+                    var = self._load_template(token["path"])
 
         return var
 
@@ -189,6 +258,40 @@ class Codegen(object):
                 self._files[path] = None
         return self._files[path]
 
+    def _get_modified_files(self):
+        results = {}
+
+        for dir in self._watched_dirs:
+            for path in glob.glob(dir + '/**', recursive=True):
+                if not os.path.isdir(path):
+                    results[path] = os.path.getmtime(path)
+
+        diff = {
+                    key: value for key, value in results.items()
+                    if key not in self._watched_files or
+                    value - self._watched_files[key] > 0
+               }
+        self._watched_files = results
+
+        curr = time.time()
+        self._recently_modified = {
+            key: value
+            for key, value in self._watched_files.items()
+            if curr - value < self._recent_ttl
+        }
+
+        return diff
+
+    def _check_recently_modified(self):
+        modified = {}
+        for path, old in self._recently_modified.items():
+            new = os.path.getmtime(path)
+            if new - old > 0.0:
+                self._recently_modified[path] = new
+                self._watched_files[path] = new
+                modified[path] = new
+        return modified
+
 
 def main():
     codegen = Codegen()
@@ -200,23 +303,34 @@ def main():
         elif arg == '-s' or arg == "--schemas":
             for schema in val.split(","):
                 codegen.add_schema(schema)
+        elif arg == "-w" or arg == "--watch":
+            for dir in val.split(","):
+                codegen.add_watched(dir)
+        elif arg == "-p" or arg == "--print":
+            codegen.print_to_stdout()
 
     for i, arg in enumerate(sys.argv):
         if i == 0:
             continue
 
-        split = arg.split("=")
-
-        if len(split) == 1:
+        if arg[0] != "-":
             filename, extension = os.path.splitext(arg)
             if extension == ".json":
                 codegen.add_schema(arg)
             elif extension == ".template":
                 codegen.add_template(arg)
         else:
-            parse_arg(split[0], split[1])
+            split = arg.split("=")
 
-    codegen.process()
+            if len(split) == 2:
+                arg = split[0]
+                val = split[1]
+            else:
+                val = None
+
+            parse_arg(arg, val)
+
+    codegen.start()
 
 
 if __name__ == "__main__":
